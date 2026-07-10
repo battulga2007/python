240937c52b8fcaf6338b74ebe734630b19bd7584
battulga2007 <g8battulga2007tulga@gmail.com>
2026-07-10 23:11:24 +0800

    Create warmup1.py

---
+++ warmup1.py
@@ -0,0 +1,65 @@
+def summ(numbers):
+    total = 0
+    for i in numbers:
+        total += i
+    print("The sum is", total)
+
+    return total
+
+def avg(numbers):
+    added_up = summ(numbers)
+    average = added_up/len(numbers)
+    print("The average is", )
+
+    return average
+
+def maximum_minimum(numbers):
+    sorted_num = sorted(numbers)
+    print("The maximum is", sorted_num[-1])
+    print("The minimum is", sorted_num[0])
+
+def count_even_odd(numbers):
+    even = 0
+    odd = 0
+
+    for i in numbers:
+        if i == 0:
+            continue
+        elif i % 2 == 0:
+            even += 1
+        else:
+            odd += 1
+
+    print("The number of odds are", odd)
+    print("The number of evens are", even)
+
+    return odd, even
+
+
+def main():
+    i = 0
+    while i == 0:
+        n = 1
+
+        count = int(input("Hello! How many inputs are you adding?: "))
+        numbers = []
+
+        while n <= count:
+            adding_num = int(input("Add input " + str(n) + ":"))
+            numbers.append(adding_num)
+            n += 1
+
+        summ(numbers)
+        avg(numbers)
+        maximum_minimum(numbers)
+        count_even_odd(numbers)
+        
+        answer = input("Would you like to close it?(Y,N): ")
+        if answer == "Y":
+            print("Bye!")
+            i += 1
+        elif answer == "N":
+            print("Ok then")
+
+            
+main()
